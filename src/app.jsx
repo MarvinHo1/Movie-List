b56dd17647c6993203b17565be12af354a1be85a
Marvin Ho <marvinho@Marvins-MacBook-Pro.local>
2019-06-21 17:21:52 -0700

    Movie-List

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,58 @@
-const movies = [
-    {id:0, title: 'Mean Girls'},
-    {id:1, title: 'Hackers'},
-    {id:2, title: 'The Grey'},
-    {id:3, title: 'Sunshine'},
-    {id:4, title: 'Ex Machina'},
-  ];
+
 
 /* 
 -add a search bar and button
 -figure out input text field
 -add someway to handle click events
+-setup "controlled component"
+  -store the text value fo the input in React State
+  -when new input is entered, first change it in the React State,
+  -then render the component based off that react state
 -add functionality to handle search */
+
+
+
 
 var Search = ()=> {
     return console.log('Marvin')
 }
 
+var typing = (text) => {
+    this.setState({inputTest: text})
+    console.log('i got it going')
+}
 
-const App = () => <div>
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state ={inputTest:''};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+handleChange(event) {
+    const inputText = event.target.value;
+    this.setState({inputText})
+console.log(event.target.value)
+}
+
+
+render(){
+    return (
+<div>
     <h1>MovieList</h1>
-    <input type="text" />
+    <input value={this.state.inputTest} type="text" onChange= {this.handleChange}/>
     <button onClick={Search}> Search </button>
         <ul>
-          {movies.map(movie => <li key = {movie.id}> {movie.title} </li>) }
+          {this.props.movies.map(movie => <li key = {movie.id}> {movie.title} </li>) }
         </ul> 
 </div>
+    )
+  }
+}
+
+// this.state.inputTest = text
+
+
 export default App
 
 
